ec47fdec3135103cda30793a78b2b108473c4c8b
oeliezergarcia <oeliezergarcia@gmail.com>
2022-10-27 12:08:21 -0300

    atualizando

--- script.js
+++ script.js
@@ -4,4 +4,38 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 const passwordConfirmation = document.getElementById('passwordConfirmation')
 
-form.addEventListener("submit", (e) 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs(){
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
+
+  if (usernameValue === "") {
+    setErrorFor(username, "O nome de usuário é obrigatório.");
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  // Adicionar a mensagem de erro
+  small.innerText = message;
+
+  // Adiciona a classe de erro
+  formControl.className = "Form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Adicionar a classe de sucesso
+  formControl.className = "form-control success";
+}
+
